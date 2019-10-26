74ab540fc731c222aaff8f9beb2df83c5b8cba19
Alex Lübeck <ndsvw@gmx.de>
2019-10-26 11:34:59 +0200

    added method add(int n)

--- src/main/java/coursera/median_maintenance/MedianMaintenance.java
+++ src/main/java/coursera/median_maintenance/MedianMaintenance.java
@@ -11,4 +11,42 @@ public class MedianMaintenance {
 		smallHalf = new PriorityQueue<Integer>(size, Collections.reverseOrder());
 		largeHalf = new PriorityQueue<Integer>(size);
 	}
+
+	public void add(int n) {
+		if (smallHalf.size() == 0 && largeHalf.size() == 0) {
+			largeHalf.add(n);
+			return;
+		}
+		if (smallHalf.size() == 0 && largeHalf.size() > 0) {
+			if (largeHalf.peek() >= n) {
+				smallHalf.add(n);
+			} else {
+				smallHalf.add(largeHalf.poll());
+				largeHalf.add(n);
+			}
+			return;
+		}
+		int x = smallHalf.peek();
+		int y = largeHalf.peek();
+		if (n <= x) {
+			// needs to be inserted into the smallHalf
+			if (smallHalf.size() > largeHalf.size()) {
+				largeHalf.add(smallHalf.poll());
+			}
+			smallHalf.add(n);
+		} else if (n >= y) {
+			// needs to be inserted into the largeHalf
+			if (largeHalf.size() > smallHalf.size()) {
+				smallHalf.add(largeHalf.poll());
+			}
+			largeHalf.add(n);
+		} else {
+			// we put it in the PrioQueue with smaller size
+			if (smallHalf.size() <= largeHalf.size()) {
+				smallHalf.add(n);
+			} else {
+				largeHalf.add(n);
+			}
+		}
+	}
 }
