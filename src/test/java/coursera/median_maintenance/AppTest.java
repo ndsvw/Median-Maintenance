3b227762b1a873dd62731a1d6190076405efa9bb
Alex Lübeck <ndsvw@gmx.de>
2019-10-27 07:59:33 +0100

    updated the test cases for generic MM

--- src/test/java/coursera/median_maintenance/AppTest.java
+++ src/test/java/coursera/median_maintenance/AppTest.java
@@ -2,38 +2,40 @@ package coursera.median_maintenance;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
-	public void test1() throws Exception {
-		MedianMaintenance mm = new MedianMaintenance(3);
-		assertEquals(mm.addAndReturnMedian(4), 4);
-		assertEquals(mm.addAndReturnMedian(10), 4);
-		assertEquals(mm.addAndReturnMedian(20), 10);
+	public void testInt1() throws Exception {
+		MedianMaintenance<Integer> mm = new MedianMaintenance<Integer>(3);
+		assertEquals((int) mm.addAndReturnMedian(4), 4);
+		assertEquals((int) mm.addAndReturnMedian(10), 4);
+		assertEquals((int) mm.addAndReturnMedian(20), 10);
 	}
-	
+
 	@Test
-	public void test2() throws Exception {
-		MedianMaintenance mm = new MedianMaintenance(10);
-		assertEquals(mm.addAndReturnMedian(0), 0);
-		assertEquals(mm.addAndReturnMedian(1), 0);
-		assertEquals(mm.addAndReturnMedian(2), 1);
-		assertEquals(mm.addAndReturnMedian(3), 1);
-		assertEquals(mm.addAndReturnMedian(4), 2);
-		assertEquals(mm.addAndReturnMedian(5), 2);
+	public void testInt2() throws Exception {
+		MedianMaintenance<Integer> mm = new MedianMaintenance<Integer>(10);
+		assertEquals((int) mm.addAndReturnMedian(0), 0);
+		assertEquals((int) mm.addAndReturnMedian(1), 0);
+		assertEquals((int) mm.addAndReturnMedian(2), 1);
+		assertEquals((int) mm.addAndReturnMedian(3), 1);
+		assertEquals((int) mm.addAndReturnMedian(4), 2);
+		assertEquals((int) mm.addAndReturnMedian(5), 2);
 	}
-	
+
 	@Test
-	public void test3() throws Exception {
-		MedianMaintenance mm = new MedianMaintenance(10);
+	public void testInt3() throws Exception {
+		MedianMaintenance<Integer> mm = new MedianMaintenance<Integer>(10);
 		mm.addAndReturnMedian(4);
 		mm.addAndReturnMedian(7);
 		mm.addAndReturnMedian(1);
 		mm.addAndReturnMedian(10);
 		mm.addAndReturnMedian(3);
-		assertEquals(mm.addAndReturnMedian(12), 4);
+		assertEquals((int) mm.addAndReturnMedian(12), 4);
 	}
 
 }
