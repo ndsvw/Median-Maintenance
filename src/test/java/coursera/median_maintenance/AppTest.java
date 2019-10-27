91f11a26018e5f1d8bb5198d427747c048a2e55b
Alex Lübeck <ndsvw@gmx.de>
2019-10-27 08:00:16 +0100

    added test cases for Long, Double and Float

--- src/test/java/coursera/median_maintenance/AppTest.java
+++ src/test/java/coursera/median_maintenance/AppTest.java
@@ -38,4 +38,34 @@ public class AppTest {
 		assertEquals((int) mm.addAndReturnMedian(12), 4);
 	}
 
+	@Test
+	public void testLong() throws Exception {
+		long[] arr = new long[] { 4, 7, 1, 3, 10, 12 };
+		long[] medians = new long[] { 4, 4, 4, 3, 4, 4 };
+		MedianMaintenance<Long> mm = new MedianMaintenance<Long>(arr.length);
+		for (int i = 0; i < 6; i++) {
+			assertEquals((long) mm.addAndReturnMedian(arr[i]), medians[i]);
+		}
+	}
+
+	@Test
+	public void testDouble() throws Exception {
+		double[] arr = new double[] { -1.1, 9.1, -2.7, -8.1, -7, 6.6, 6.8 };
+		double[] medians = new double[] { -1.1, -1.1, -1.1, -2.7, -2.7, -2.7, -1.1 };
+		MedianMaintenance<Double> mm = new MedianMaintenance<Double>(arr.length);
+		for (int i = 0; i < 7; i++) {
+			assertEquals((double) mm.addAndReturnMedian(arr[i]), medians[i], 0.01);
+		}
+	}
+
+	@Test
+	public void testFloat() throws Exception {
+		float[] arr = new float[] { -1.1f, 9.1f, -2.7f, -8.1f, -7f, 6.6f, 6.8f };
+		float[] medians = new float[] { -1.1f, -1.1f, -1.1f, -2.7f, -2.7f, -2.7f, -1.1f };
+		MedianMaintenance<Float> mm = new MedianMaintenance<Float>(arr.length);
+		for (int i = 0; i < 7; i++) {
+			assertEquals((float) mm.addAndReturnMedian(arr[i]), medians[i], 0.01);
+		}
+	}
+
 }
